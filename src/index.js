fd28be17a6baaf6446acac3a30100a85b8899547
Dmitry Korobka <dbox.insight@gmail.com>
2023-01-15 13:25:32 +0300

    fix: complete option list logic

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import './style.scss'
 import './welcome.js'
-import Data from './db_detective.js'
+import Data from './db_detective.js';
 
 const options_list = document.getElementsByClassName('option-list');
 const options = document.querySelectorAll('.option');
+const navHeader = document.querySelector('.ul-nav');
+const scorePoint = document.querySelector('.score');
+const navHeader2 = document.getElementsByClassName('ul-nav');
 const next_button = document.querySelector('.next');
+const img = document.querySelector('.poster');
 let step = 0;
+let score = 10;
+let btnLock = false;
 
 const query = []
 
@@ -17,30 +23,44 @@ while (query.length < 10) {
 }
 
 function getRandomWinOption(step) {
-    return Data[step][Math.ceil(Math.random()*6)];
+    return Data[step][Math.ceil(Math.random()*6) - 1];
 }
 
 let winOption = getRandomWinOption(step);
 
 function optionClick(event) {
-    //event.target.classList.add('button_active');
-    event.target.innerHTML == winOption.name ? 
-        event.target.classList.add('right') :
-        event.target.classList.add('wrong');
-    // event.target.addEventListener('transitionend', () => {
-        
-    // })  
+    if (!btnLock) {
+        if (event.target.innerHTML == winOption.name) {
+            event.target.classList.add('right');
+            scorePoint.innerHTML = `Score: ${score}`;
+            btnLock = true;
+            img.src = winOption.image;
+        } else {
+            event.target.classList.add('wrong');
+            score -= 2;
+        }
+    }  
 }
 
 function nextClick() {
-    step++;
-    winOption = getRandomWinOption(step);
-    for (let i = 0; i < 6; i++) {
-        options[i].classList.remove('right');
-        options[i].classList.remove('wrong');
-        options[i].innerHTML = Data[step][i].name;
+    if (step < 5) {
+        navHeader.children[step].classList.remove('active');
+        step++;
+        score += 10;
+        btnLock = false;
+        navHeader.children[step].classList.add('active');
+        winOption = getRandomWinOption(step);
+        console.log('next: ' + winOption);
+
+        for (let i = 0; i < 6; i++) {
+            options[i].classList.remove('right');
+            options[i].classList.remove('wrong');
+            options[i].innerHTML = Data[step][i].name;
+        }
+    } else {
+        navHeader.children[step].classList.remove('active');
+
     }
-    
 }
 
 function init() {    
@@ -54,3 +74,4 @@ function init() {
 
 init();
 console.log(query);
+console.log(img.src);
